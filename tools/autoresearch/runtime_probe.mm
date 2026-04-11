020b8651c69463e984d8db5733bf84430176a03e
Buseong Kim <flight@skyline23.com>
2026-04-11 17:16:32 +0900

    Revert "exp431: ignore coalesced capture events"
    This reverts commit 26d1bb218dde7284fc1e0e397c9c43eb2c877b69.

--- tools/autoresearch/runtime_probe.mm
+++ tools/autoresearch/runtime_probe.mm
@@ -188,8 +188,6 @@ void drainForwardedEvents(LumenMacBridgeController *controller, ProbeMetrics &me
       case LumenCoreCaptureEventKindFailed:
         metrics.failureEvents += 1;
         break;
-      case LumenCoreCaptureEventKindCoalescedFrame:
-        break;
       case LumenCoreCaptureEventKindDroppedFrame:
         metrics.dropEvents += 1;
         break;
