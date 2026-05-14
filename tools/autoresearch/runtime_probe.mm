aa9fb61bdd0e8e5327ba3c7c798e39be4783d1f0
Buseong Kim <flight@skyline23.com>
2026-05-14 21:39:13 +0900

    Revert "Experiment 2119 request warmup shared cadence probe"
    This reverts commit 6863c0e140e5182a5fa7188b5592b51cb280f668.

--- tools/autoresearch/runtime_probe.mm
+++ tools/autoresearch/runtime_probe.mm
@@ -7,7 +7,6 @@
 
 #include <algorithm>
 #include <chrono>
-#include <cstdlib>
 #include <cstdint>
 #include <cstdio>
 #include <cstring>
@@ -145,13 +144,6 @@ void emitSelectedDiagnostics(const char *diagnostics) {
     {"skyLightSyntheticIdleReplayIntervalMilliseconds", "SKYLIGHT_SYNTHETIC_IDLE_REPLAY_INTERVAL_MS"},
     {"skyLightPendingPolicy", "SKYLIGHT_PENDING_POLICY"},
     {"skyLightRecommendedPendingFrameCount", "SKYLIGHT_RECOMMENDED_PENDING_FRAME_COUNT"},
-    {"skyLightPreflightWarmupStatus", "SKYLIGHT_PREFLIGHT_WARMUP_STATUS"},
-    {"skyLightPreflightWarmupStopStatus", "SKYLIGHT_PREFLIGHT_WARMUP_STOP_STATUS"},
-    {"skyLightPreflightWarmupCallbackCount", "SKYLIGHT_PREFLIGHT_WARMUP_CALLBACK_COUNT"},
-    {"skyLightPreflightWarmupCompleteFrameCount", "SKYLIGHT_PREFLIGHT_WARMUP_COMPLETE_FRAME_COUNT"},
-    {"skyLightPreflightWarmupObservedFrameRate", "SKYLIGHT_PREFLIGHT_WARMUP_OBSERVED_FRAME_RATE"},
-    {"skyLightPreflightWarmupCadence", "SKYLIGHT_PREFLIGHT_WARMUP_CADENCE"},
-    {"skyLightPreflightWarmupError", "SKYLIGHT_PREFLIGHT_WARMUP_ERROR"},
     {"sourceFrameCount", "SOURCE_FRAME_COUNT"},
     {"sourceDisplayDeltaCount", "SOURCE_DISPLAY_DELTA_COUNT"},
     {"sourceLastDisplayDeltaMilliseconds", "SOURCE_LAST_DISPLAY_DELTA_MS"},
@@ -344,7 +336,6 @@ int main(int argc, const char *argv[]) {
     int32_t fps = std::atoi(argv[3]);
     LumenCoreCaptureCodec codec = codecFromArgument(argc >= 5 ? argv[4] : nullptr);
     uint32_t displayID = CGMainDisplayID();
-    setenv("MDK_SKYLIGHT_DISPLAY_STREAM_PREFLIGHT_WARMUP", "1", 1);
 
     LumenMacBridgeController *controller = LumenMacBridgeControllerCreate();
     if (controller == nullptr) {
